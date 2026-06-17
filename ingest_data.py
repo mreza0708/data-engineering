6af3cad3b5b438b86c9d04e64c75efe8f605e398
mreza <mrezaheydari.0708@gmail.com>
2026-06-17 19:18:44 +0330

    click-solveing

--- ingest_data.py
+++ ingest_data.py
@@ -3,6 +3,26 @@ from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 import click
 
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64",
+}
+
+parse_dates = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
 
 @click.command()
 @click.option("--pg-user", default="root", help="PostgreSQL username")
@@ -14,66 +34,42 @@ import click
 @click.option("--month", default=1, type=int, help="Month of the data")
 @click.option("--chunksize", default=100000, type=int, help="Chunk size for ingestion")
 @click.option("--target-table", default="yellow_taxi_data", help="Target table name")
-def main(
-    pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table
-):
-
-    dtype = {
-        "VendorID": "Int64",
-        "passenger_count": "Int64",
-        "trip_distance": "float64",
-        "RatecodeID": "Int64",
-        "store_and_fwd_flag": "string",
-        "PULocationID": "Int64",
-        "DOLocationID": "Int64",
-        "payment_type": "Int64",
-        "fare_amount": "float64",
-        "extra": "float64",
-        "mta_tax": "float64",
-        "tip_amount": "float64",
-        "tolls_amount": "float64",
-        "improvement_surcharge": "float64",
-        "total_amount": "float64",
-        "congestion_surcharge": "float64",
-    }
-
-    parse_dates = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
-
-
-def run():
-    pg_user = "root"
-    pg_pass = "root"
-    pg_host = "localhost"
-    pg_port = "15432"
-    pg_db = "ny_taxi"
-    year = 2021
-    month = 1
-    chunksize = 100000
-    target_table = "yellow_taxi_data"
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
 
     engine = create_engine(
         f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
     )
 
-    url_prefix = (
-        "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow"
-    )
+    url_prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow"
     url = f"{url_prefix}/yellow_tripdata_{year:04d}-{month:02d}.csv.gz"
 
     df_iter = pd.read_csv(
-        url, dtype=dtype, parse_dates=parse_dates, iterator=True, chunksize=chunksize
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize,
+        compression="gzip",
     )
 
     first = True
 
     for df_chunk in tqdm(df_iter):
         if first:
-            df_chunk.head(n=0).to_sql(
-                name=target_table, con=engine, if_exists="replace"
+            df_chunk.head(0).to_sql(
+                name=target_table,
+                con=engine,
+                if_exists="replace",
+                index=False
             )
             first = False
 
-        df_chunk.to_sql(name=target_table, con=engine, if_exists="append")
+        df_chunk.to_sql(
+            name=target_table,
+            con=engine,
+            if_exists="append",
+            index=False
+        )
 
 
 if __name__ == "__main__":
